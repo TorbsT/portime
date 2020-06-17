6c5377d6e75e4a0cd0e7029ef34ccd3762818020
TorbsT <37838405+TorbsT@users.noreply.github.com>
2020-06-17 03:35:17 +0200

    WIP Portals
    h

---
+++ PORTIME/Assets/Portal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] GameObject exit;
+    [SerializeField] GameObject plane;
+
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject collider = collision.gameObject;
+        collider.transform.position = exit.transform.position;
+        collider.transform.Rotate(0f, exit.transform.rotation.x-collider.transform.rotation.y, 0f);
+    }
+}
