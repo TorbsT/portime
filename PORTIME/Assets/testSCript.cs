3fbbc6745485c5b726a89339732dccfcbf552465
TorbsT <37838405+TorbsT@users.noreply.github.com>
2021-03-14 20:57:59 +0100

    Factory reset PC
    yup

---
+++ PORTIME/Assets/testSCript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class testSCript : MonoBehaviour
+{
+    Text txt;
+    int i;
+    // Update is called once per frame
+    void Start()
+    {
+        txt = GetComponent<Text>();
+        txt.text = "";
+    }
+    void Update()
+    {
+        i++;
+        if (i > 1f/Time.deltaTime)
+        {
+            txt.text += "a";
+            i = 0;
+        }
+    }
+}
